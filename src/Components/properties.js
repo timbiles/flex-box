b0cd15fa36d42648b9970ba6d4014605d48cefbf
Tim Biles <timbilestimbiles@gmail.com>
2018-11-07 16:16:12 -0600

    add columns

--- src/Components/properties.js
+++ src/Components/properties.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 import flex from '../data/flex.json';
 
+const Header = styled.div`
+  height: 5vh;
+  background: grey;
+  font-size: 2em;
+`;
+
 const Container = styled.div`
   display: flex;
+  height: 95vh;
 
-  ${props => props.primary && css`
-    width: 100vw;
-    flex-direction: column;
-    justify-content: flex-start;
-  `}
+  ${props =>
+    props.primary &&
+    css`
+      flex-direction: column;
+      flex: 1;
+    `}
+  ${props =>
+    props.secondary &&
+    css`
+      flex-direction: column;
+      justify-content: flex-start;
+    `}
 `;
 
 const Flex = styled.div`
   display: flex;
+  flex: 3;
   flex-direction: ${props => props.direction};
   flex-wrap: ${props => props.wrap};
   justify-content: ${props => props.justifyContent};
@@ -23,73 +38,87 @@ const Flex = styled.div`
 
 const P = styled.p`
   border: 1px solid #000;
+  padding: 5%;
 `;
 
 class properties extends Component {
-    state = {}
+  state = {
+      number: 4
+  };
 
-    componentDidMount() {
-        this.setState(flex)
-        console.log('hit')
-    }
+  componentDidMount() {
+    this.setState(flex);
+  }
 
   map = (val, name) => {
-      console.log(this.state)
-      
     return (
-      <Container primary>
+      <Container secondary>
         <h2>{val && val.name}</h2>
-        {val && val.arr.map((e, i) => {
-          return (
-            <div key={i}>
-              <input
-                name={name}
-                type="radio"
-                value={e}
-                onChange={e => {
-                  this.setState({
-                    [name]: { ...val, value: e.target.value }
-                  });
-                }}
-              />
-              <label htmlFor="">{e}</label>
-            </div>
-          );
-        })}
+        {val &&
+          val.arr.map((e, i) => {
+            return (
+              <div key={i}>
+                <input
+                  name={name}
+                  type="radio"
+                  value={e}
+                  onChange={e => {
+                    this.setState({
+                      [name]: { ...val, value: e.target.value }
+                    });
+                  }}
+                />
+                <label htmlFor="">{e}</label>
+              </div>
+            );
+          })}
       </Container>
     );
   };
 
+  pLoop = (num) => {
+      console.log(num)
+      const arr = []
+    for(let i = 1; i<=num; i++){
+      arr.push(i)
+    }
+    return arr.map((e, i) => <P key={i}>{e}</P>)
+}
+
   render() {
-    console.log(this.state);
     const {
       direction,
       wrap,
       justifyContent,
       alignItems,
-      alignContent
+      alignContent,
+      number
     } = this.state;
+
 
     return (
       <>
+        <Header>
+            Flex Box Playground
+        </Header>
         <Container>
-          {this.map(direction, 'direction')}
-          {this.map(wrap, 'wrap')}
-          {this.map(justifyContent, 'justifyContent')}
-          {this.map(alignItems, 'alignItems')}
-          {this.map(alignContent, 'alignContent')}
+          <Container primary>
+            {this.map(direction, 'direction')}
+            {this.map(wrap, 'wrap')}
+            {this.map(justifyContent, 'justifyContent')}
+            {this.map(alignItems, 'alignItems')}
+            {this.map(alignContent, 'alignContent')}
+            <button onClick={() => this.setState({number: number + 1})}>Add Number</button>
+          </Container>
+          <Flex
+            direction={direction && direction.value}
+            wrap={wrap && wrap.value}
+            justifyContent={justifyContent && justifyContent.value}
+            alignItems={alignItems && alignItems.value}
+          >
+          {this.pLoop(number)}
+          </Flex>
         </Container>
-        <Flex
-          direction={direction && direction.value}
-          wrap={wrap && wrap.value}
-          justifyContent={justifyContent && justifyContent.value}
-          alignItems={alignItems && alignItems.value}
-        >
-          <P>1</P>
-          <P>2</P>
-          <P>3</P>
-          <P>4</P>
-        </Flex>
       </>
     );
   }
