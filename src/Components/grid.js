2602fe2369f3f3476221eae8b90b33c5956acc28
Tim Biles <timbilestimbiles@gmail.com>
2018-11-10 20:36:21 -0600

    added grid styling

--- src/Components/grid.js
+++ src/Components/grid.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import {Container } from '../styles/gridStyles'; 
+import { Grid } from '../styles/gridStyles'; 
 
 class grid extends Component {
     render() {
         return (
             <>
+                <Grid>
                 <h1>Grid</h1>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                </Grid>
+
             </>
         );
     }
--- src/styles/gridStyles.js
+++ src/styles/gridStyles.js
@@ -2,4 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const Grid = styled.div`
     display: grid;
-`
+    grid-template-columns: 1fr 1fr;
+    /* grid-template-rows: 1fr 1fr 1fr 1fr; */
+}
+`;
